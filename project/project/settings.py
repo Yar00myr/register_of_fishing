0fc14a48462c76036211cbefde0b69136c4bdbbe
Yar00myr <ivanucaromir@gmail.com>
2026-08-28 23:59:19 +0100

    feat: add CSRF_TRUSTED_ORIGINS, secure cookies, fix timezone

--- project/project/settings.py
+++ project/project/settings.py
@@ -29,7 +29,7 @@ load_dotenv()
 SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".trycloudflare.com", "*"]
 
@@ -89,23 +89,23 @@ STORAGES = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": getenv("POSTGRES_DB"),
-#         "USER": getenv("POSTGRES_USER"),
-#         "PASSWORD": getenv("POSTGRES_PASSWORD"),
-#         "HOST": getenv("POSTGRES_HOST"),
-#         "PORT": "5432",
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+     "default": {
+         "ENGINE": "django.db.backends.postgresql",
+         "NAME": getenv("POSTGRES_DB"),
+         "USER": getenv("POSTGRES_USER"),
+         "PASSWORD": getenv("POSTGRES_PASSWORD"),
+         "HOST": getenv("POSTGRES_HOST"),
+         "PORT": "5432",
+     }
 }
+
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 
 
 # Password validation
