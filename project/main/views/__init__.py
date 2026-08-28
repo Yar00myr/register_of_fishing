8c5b2f301fafab65a6df8cd8513155b547b04bd9
Yar00myr <ivanucaromir@gmail.com>
2026-08-28 15:24:41 +0300

    refactor: format files

--- project/main/views/__init__.py
+++ project/main/views/__init__.py
@@ -1,14 +1,14 @@
 from .views import (
-    login_page as login_page, 
-    logout_page as logout_page, 
-    homepage_view as homepage_view, 
-    new_fish_type as new_fish_type
+    login_page as login_page,
+    logout_page as logout_page,
+    homepage_view as homepage_view,
+    new_fish_type as new_fish_type,
 )
 from .trips import (
-    add_trip_view as add_trip_view, 
-    trips_list_view as trips_list_view, 
-    trip_detail_view as trip_detail_view, 
-    delete_trip as delete_trip, 
+    add_trip_view as add_trip_view,
+    trips_list_view as trips_list_view,
+    trip_detail_view as trip_detail_view,
+    delete_trip as delete_trip,
     edit_trip as edit_trip,
-    catch_photos_view as catch_photos_view
+    catch_photos_view as catch_photos_view,
 )
